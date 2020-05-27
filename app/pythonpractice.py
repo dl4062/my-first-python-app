b51fe5e794c49dad585679af6eba90346ca2ecf1
dl4062 <dl4062@stern.nyu.edu>
2020-05-26 20:54:09 -0400

    Create pythonpractice.py

---
+++ app/pythonpractice.py
@@ -0,0 +1,28 @@
+x=1
+print(x)
+
+x=10
+print(x)
+
+x=x+100
+print(x)
+
+breakpoint()
+
+
+x=5
+
+x+=100 #equiv shorthand for x=x+100
+print(x)
+
+print()
+
+print(1, 2, 3)  ## print values separated by commas
+
+
+z = 12
+
+if z==37:       # need double equals in order for it to be a geometrics equal, otherwise you are just changing the variable
+    print("YES")
+else:
+    print("NO")
